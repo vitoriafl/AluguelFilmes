244acb8fd520577e0b95d2ba1741c73279ccd1bf
dudu <mattosdudu2009@gmail.com>
2024-05-21 20:00:38 -0300

    Tratamento de execoes em CLientes finalizadas

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Cliente extends Pessoa implements IPessoa{
     //Variaveis
 
@@ -10,42 +12,67 @@ public class Cliente extends Pessoa implements IPessoa{
     private String email;
     private static int contadorC; //contador de clientes
 
-    //Construtores
+    private static ArrayList<Cliente> clientes = new ArrayList<>();
+
+    //Construtores:
 
     public Cliente(String nome, int idade, int cpf, int telefone, int contadorFilmesAlugados, String endereco, String email) {
-        this.nome = nome;
-        this.idade = idade;
-        this.cpf = cpf;
-        this.telefone = telefone;
-        this.contadorFilmesAlugados = contadorFilmesAlugados;
-        this.endereco = endereco;
-        this.email = email;
+        //usei set pq ele ja vem com o tratamento de execoes
+        setNome(nome);
+        setIdade(idade);
+        setCpf(cpf);
+        setTelefone(telefone);
+        setContadorFilmesAlugados(contadorFilmesAlugados);
+        setEndereco(endereco);
+        setEmail(email);
     }
 
     public Cliente() {
     }
 
-    //Metodos
+    //Metodos:
 
-    //Exibe todos os dados do Cliente
+    //cadastra o cliente no array
+    public void cadastrarCliente(String nome, int idade, int cpf, int telefone, int contadorFilmesAlugados, String endereco, String email) {
+        try{
+            Cliente cliente = new Cliente(nome,idade,cpf,telefone,contadorFilmesAlugados,endereco,email);
+            clientes.add(cliente);
+            Cliente.contadorC++;
+        }catch (IllegalArgumentException e){
+            System.out.println("Um erro aconteceu, tente novamente");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void removerCliente(int cpf){
+        //Esse metodo deu erro no da Maria, testar dps pra ver se no nosso da certo
+        for(Cliente C : Cliente.clientes){
+            if(C.cpf == cpf){
+                Cliente.clientes.remove(clientes.indexOf(C));
+            }
+        }
+    }
+
+    //exibe todos os dados do Cliente
     @Override
     public void exibir() {
         System.out.println("Num de clientes: " + this.getCpf());
         System.out.println("Nome: " + this.getNome());
-        System.out.println("Idade: " + this.getIdade());System.out.println("Endereço: " + this.getEndereco());
+        System.out.println("Idade: " + this.getIdade());
+        System.out.println("Endereço: " + this.getEndereco());
         System.out.println("Telefone: " + this.getTelefone());
         System.out.println("Email: " + this.getEmail());
         System.out.println("Num de filmes alugados: " + this.getContadorFilmesAlugados());
     }
 
-    //Getters e Setters
+    //Getters e Setters:
 
+    //todos ja estao com tratamento de execoes
     public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) {
-        //FEITO TRATAMENTO DE EXECOES
         if(nome.isEmpty()){
             throw new IllegalArgumentException("Nome precisa ser preenchido");
         } else{
@@ -58,8 +85,11 @@ public class Cliente extends Pessoa implements IPessoa{
     }
 
     public void setIdade(int idade) {
-        //A FAZER TRATAMENTO DE EXEÇOES
-        this.idade = idade;
+        if(idade==0 | idade<0){
+            throw new IllegalArgumentException("Idade precisa ser maior ou diferente de zero");
+        } else{
+            this.idade = idade;
+        }
     }
 
     public int getCpf() {
@@ -67,8 +97,11 @@ public class Cliente extends Pessoa implements IPessoa{
     }
 
     public void setCpf(int cpf) {
-        //A FAZER TRATAMENTO DE EXEÇOES
-        this.cpf = cpf;
+        if(cpf==0 | cpf<0){
+            throw new IllegalArgumentException("Cpf precisa ser maior ou diferente de zero");
+        } else {
+            this.cpf = cpf;
+        }
     }
 
     public int getContadorFilmesAlugados() {
@@ -76,8 +109,11 @@ public class Cliente extends Pessoa implements IPessoa{
     }
 
     public void setContadorFilmesAlugados(int contadorFilmesAlugados) {
-        //A FAZER TRATAMENTO DE EXEÇOES
-        this.contadorFilmesAlugados = contadorFilmesAlugados;
+        if(contadorFilmesAlugados<0){
+            throw new IllegalArgumentException("Contador precisa ser maior ou igual a zero");
+        } else {
+            this.contadorFilmesAlugados = contadorFilmesAlugados;
+        }
     }
 
     public static int getContadorC() {
@@ -85,8 +121,11 @@ public class Cliente extends Pessoa implements IPessoa{
     }
 
     public static void setContadorC(int contadorC) {
-        //A FAZER TRATAMENTO DE EXEÇOES
-        Cliente.contadorC = contadorC;
+        if(contadorC<0){
+            throw new IllegalArgumentException("Contador precisa ser maior ou igual a zero");
+        } else {
+            Cliente.contadorC = contadorC;
+        }
     }
 
     public int getTelefone() {
@@ -94,8 +133,11 @@ public class Cliente extends Pessoa implements IPessoa{
     }
 
     public void setTelefone(int telefone) {
-        //A FAZER TRATAMENTO DE EXEÇOES
-        this.telefone = telefone;
+        if(telefone==0 | telefone<0){
+            throw new IllegalArgumentException("Telefone precisa ser maior ou  diferente de zero");
+        } else {
+            this.telefone = telefone;
+        }
     }
 
     public String getEndereco() {
@@ -103,7 +145,6 @@ public class Cliente extends Pessoa implements IPessoa{
     }
 
     public void setEndereco(String endereco) {
-        //FEITO TRATAMENTO DE EXECOES
         if(endereco.isEmpty()){
             throw new IllegalArgumentException("Endereço precisa ser preenchido");
         } else{
@@ -116,13 +157,10 @@ public class Cliente extends Pessoa implements IPessoa{
     }
 
     public void setEmail(String email) {
-        //FEITO TRATAMENTO DE EXECOES
         if(email.isEmpty()){
             throw new IllegalArgumentException("Email precisa ser preenchido");
         } else{
             this.email = email;
         }
     }
-
-
 }
