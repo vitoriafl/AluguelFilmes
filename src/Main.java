72a7dd5210bdd169ea000f6b97ccaf160956c178
dudu <mattosdudu2009@gmail.com>
2024-05-21 21:10:47 -0300

    menu basico, switch case e while

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input =new Scanner(System.in);
-        int escolha
-        System.out.println("Hello world");
+        int escolha = -1;
 
+        while (escolha != 0){
+            System.out.println("---------Menu--------");
+            System.out.println("1 : Busca");
+            System.out.println("2 : Idades");
+            System.out.println("3 : Preco de produtos");
+            System.out.println("0 : Sair");
+            escolha = input.nextInt();
 
+            switch (escolha){
+                case 1:
+                    System.out.println(" ");
+                    break;
+                default:
+                    System.out.println("Escolha invalida");
+                    break;
+            }
+
+        }
     }
 }
